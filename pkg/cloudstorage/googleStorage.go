3f4897a1e9d4d88c73098c4c13480d293e2bcf3d
regnivon <qdnovinger@gmail.com>
2021-03-09 18:50:22 -0500

    complete interface to google cloud storage and integration test of the server functionality

--- pkg/cloudstorage/googleStorage.go
+++ pkg/cloudstorage/googleStorage.go
@@ -3,7 +3,10 @@ package cloudstorage
 import (
 	"bytes"
 	"context"
+	"fmt"
+	"io/ioutil"
 	"os"
+	"time"
 
 	"cloud.google.com/go/storage"
 	"go.uber.org/zap"
@@ -19,7 +22,7 @@ type GoogleStorageBucket struct {
 }
 
 // NewGoogleStorageBucket initializes a GoogleStorageBucket
-func NewGoogleStorageBucket(bucketName string) (*GoogleStorageBucket, error) {
+func NewGoogleStorageBucket(bucketName string, logger *zap.SugaredLogger) (*GoogleStorageBucket, error) {
 	credPath := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
 	client, err := storage.NewClient(context.TODO(), option.WithCredentialsFile(credPath))
 
@@ -27,7 +30,7 @@ func NewGoogleStorageBucket(bucketName string) (*GoogleStorageBucket, error) {
 		return nil, err
 	}
 
-	return &GoogleStorageBucket{client: client, bucketName: bucketName}, nil
+	return &GoogleStorageBucket{client: client, bucketName: bucketName, logger: logger}, nil
 }
 
 // Close - closes the Client. Close need not be called at program exit.
@@ -36,16 +39,38 @@ func (g *GoogleStorageBucket) Close() error {
 }
 
 func (g *GoogleStorageBucket) UploadFile(fileName string, bytes bytes.Buffer) error {
-	/*
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
-		defer cancel()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute*2)
+	defer cancel()
 
-		objectWriter := g.client.Bucket(g.bucketName).Object(fileName).NewWriter(ctx)
-	*/
+	objectWriter := g.client.Bucket(g.bucketName).Object(fileName).NewWriter(ctx)
+
+	defer objectWriter.Close()
+
+	n, err := objectWriter.Write(bytes.Bytes())
+
+	if err != nil {
+		g.logger.Debugf("Error writing to gcloud bucket: %v", err)
+		return err
+	}
+
+	g.logger.Infof("Uploaded %v bytes in %v file to gcloud", n, fileName)
 
 	return nil
 }
 
 func (g *GoogleStorageBucket) DownloadFile(fileName string) (bytes.Buffer, error) {
-	return bytes.Buffer{}, nil
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*50)
+	defer cancel()
+
+	rc, err := g.client.Bucket(g.bucketName).Object(fileName).NewReader(ctx)
+	if err != nil {
+		return bytes.Buffer{}, fmt.Errorf("Object(%q).NewReader: %v", fileName, err)
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return bytes.Buffer{}, fmt.Errorf("ioutil.ReadAll: %v", err)
+	}
+	return *bytes.NewBuffer(data), nil
 }
